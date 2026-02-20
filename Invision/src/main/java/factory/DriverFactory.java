c70dae773c3aae90bd3d19ceabfe107f69bb1e02
rohit kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-02-20 09:44:57 +0530

    Update DriverFactory.java

--- Invision/src/main/java/factory/DriverFactory.java
+++ Invision/src/main/java/factory/DriverFactory.java
@@ -14,32 +14,3 @@ public class DriverFactory {
     }
 }
 
-
-
-//package factory;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//public class DriverFactory {
-//
-//    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-//
-//    public static void initDriver(String browser) {
-//
-//        if (browser.equalsIgnoreCase("chrome")) {
-//            driver.set(new ChromeDriver());
-//        }
-//
-//        getDriver().manage().window().maximize();
-//    }
-//
-//    public static WebDriver getDriver() {
-//        return driver.get();
-//    }
-//
-//    public static void quitDriver() {
-//        getDriver().quit();
-//        driver.remove();
-//    }
-//}
