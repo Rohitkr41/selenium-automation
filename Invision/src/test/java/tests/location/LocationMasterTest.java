b7876123aa146d05881ff4365dda48f4da178fe6
rohit kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-02-20 09:48:08 +0530

    Update LocationMasterTest.java

--- Invision/src/test/java/tests/location/LocationMasterTest.java
+++ Invision/src/test/java/tests/location/LocationMasterTest.java
@@ -1,31 +1,3 @@
-//package tests.location;
-//
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-//import base.BaseTest;
-//import pages.LoginPage;
-//import pages.location.LocationMasterPage;
-//import utils.ConfigReader;
-//
-//public class LocationMasterTest extends BaseTest {
-//
-//    @Test
-//    public void verifyLocationMasterDropdown() {
-//
-//        LoginPage login = new LoginPage(driver);
-//        login.login(
-//                ConfigReader.getProperty("username"),
-//                ConfigReader.getProperty("password"));
-//
-//        LocationMasterPage location = new LocationMasterPage(driver);
-//        location.clickLocationMasters();
-//
-//        Assert.assertTrue(location.isCountryVisible(),
-//                "Dropdown not working!");
-//    }
-//}
-
 
 package tests.location;
 
