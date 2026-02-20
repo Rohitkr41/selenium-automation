eae483ef715b8aae7e3bd1a6cd80d5b49bd276d4
rohit kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-02-20 09:46:03 +0530

    Update ConfigReader.java

--- Invision/src/main/java/utils/ConfigReader.java
+++ Invision/src/main/java/utils/ConfigReader.java
@@ -22,38 +22,3 @@ public class ConfigReader {
         return properties.getProperty(key);
     }
 }
-
-
-
-
-//package utils;
-//
-//import java.io.InputStream;
-//import java.util.Properties;
-//
-//public class ConfigReader {
-//
-//    private static Properties prop = new Properties();
-//
-//    static {
-//        try {
-//            InputStream input = ConfigReader.class
-//                    .getClassLoader()
-//                    .getResourceAsStream("config.properties");
-//
-//            if (input == null) {
-//                System.out.println("❌ config.properties file NOT FOUND");
-//            } else {
-//                prop.load(input);
-//                System.out.println("✅ config.properties loaded successfully");
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static String getProperty(String key) {
-//        return prop.getProperty(key);
-//    }
-//}
