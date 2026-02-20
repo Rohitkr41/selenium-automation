1b766f15b30a0b94245d69f60fe7655d593a861c
rohit kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-02-20 09:44:04 +0530

    Update BaseTest.java

--- Invision/src/test/java/base/BaseTest.java
+++ Invision/src/test/java/base/BaseTest.java
@@ -36,36 +36,3 @@ public class BaseTest {
 
 
 
-//package base;
-//
-//import java.time.Duration;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.annotations.AfterMethod;
-//import org.testng.annotations.BeforeMethod;
-//
-//import utils.ConfigReader;
-//
-//public class BaseTest {
-//
-//    protected WebDriver driver;
-//
-//    @BeforeMethod
-//    public void setUp() {
-//
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        // ✅ Correct way
-//        driver.get(ConfigReader.getProperty("baseUrl"));
-//    }
-//
-//    @AfterMethod
-//    public void tearDown() {
-//
-//        // Debug mode (browser open rahega)
-//        // driver.quit();
-//    }
-//}
